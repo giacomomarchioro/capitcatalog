088054175341c2e9a77ac176c810014ac8cf0471
giacomounivr <giacomo.marchioro@univr.it>
2021-04-23 08:12:47 +0200

    Rimossi tutti i validatori

--- app.py
+++ app.py
@@ -32,87 +32,87 @@ db=client.admin
 
 class Biblio_int_libri(Form):
     id_biblib = StringField("Id",
-        validators=[ validators.Length(max=500)],render_kw={'class':"form-control",}
+        validators=[ ],render_kw={'class':"form-control",}
     )
     titolo = StringField("Titolo",
             render_kw={'class':"form-control",}
         )
     anno = StringField("Anno",
-            validators=[ validators.Length(max=500)],render_kw={'class':"form-control",}
+            validators=[ ],render_kw={'class':"form-control",}
         )
     numero_pagine = StringField("Numero pagine",
-            validators=[ validators.Length(max=500)],render_kw={'class':"form-control",}
+            validators=[ ],render_kw={'class':"form-control",}
         )
     citta = StringField("Citta",
-            validators=[ validators.Length(max=500)],render_kw={'class':"form-control",}
+            validators=[ ],render_kw={'class':"form-control",}
         )
     casa_editrice = StringField("Casa editrice",
-            validators=[ validators.Length(max=500)],render_kw={'class':"form-control",}
+            validators=[ ],render_kw={'class':"form-control",}
         )
     collana = StringField("Collana",
-            validators=[ validators.Length(max=500)],render_kw={'class':"form-control",}
+            validators=[ ],render_kw={'class':"form-control",}
         )
     n_collana = StringField("N collana",
-            validators=[ validators.Length(max=500)],render_kw={'class':"form-control",}
+            validators=[ ],render_kw={'class':"form-control",}
         )
     autore_nome_cognome = StringField("Autore nome cognome",
-            validators=[ validators.Length(max=500)],render_kw={'class':"form-control",}
+            validators=[ ],render_kw={'class':"form-control",}
         )
     Descrizione_interna_id = StringField("Descrizione interna id",
-            validators=[ validators.Length(max=500)],render_kw={'class':"form-control",}
+            validators=[ ],render_kw={'class':"form-control",}
     )
 
 
 class Storia_del_manoscritto(Form):
     Id_auto_inc = StringField("Id auto inc",
-        validators=[ validators.Length(max=500)],render_kw={'class':"form-control",}
+        validators=[ ],render_kw={'class':"form-control",}
     )
     Id_AT = StringField("Id",
-            validators=[ validators.Length(max=500)],render_kw={'class':"form-control",}
+            validators=[ ],render_kw={'class':"form-control",}
         )
     intervallo_carte = StringField("Intervallo carte",
-            validators=[ validators.Length(max=500)],render_kw={'class':"form-control",}
+            validators=[ ],render_kw={'class':"form-control",}
         )
     Datazione = StringField("Datazione",
-            validators=[ validators.Length(max=500)],render_kw={'class':"form-control",}
+            validators=[ ],render_kw={'class':"form-control",}
         )
     Contenuto = StringField("Contenuto",
-            validators=[ validators.Length(max=500)],render_kw={'class':"form-control",}
+            validators=[ ],render_kw={'class':"form-control",}
         )
     Posizione = StringField("Posizione",
-            validators=[ validators.Length(max=500)],render_kw={'class':"form-control",}
+            validators=[ ],render_kw={'class':"form-control",}
         )
     Tipologia_scrittura = StringField("Tipologia scrittura",
-            validators=[ validators.Length(max=500)],render_kw={'class':"form-control",}
+            validators=[ ],render_kw={'class':"form-control",}
         )
     Descrizione_Esterna_Segnatura = StringField("Descrizione esterna segnatura",
-            validators=[ validators.Length(max=500)],render_kw={'class':"form-control",}
+            validators=[ ],render_kw={'class':"form-control",}
         )
 
 class AnnotazioniMarg(Form):
     Id_auto_inc = StringField("Id auto inc",
-        validators=[ validators.Length(max=500)],render_kw={'class':"form-control",}
+        validators=[ ],render_kw={'class':"form-control",}
     )
     Id_anno = StringField("ID annotazioni marginali",
-            validators=[ validators.Length(max=500)],render_kw={'class':"form-control",}
+            validators=[ ],render_kw={'class':"form-control",}
         )
     intervallo_carte = StringField("Intervallo carte",
-            validators=[ validators.Length(max=500)],render_kw={'class':"form-control",}
+            validators=[ ],render_kw={'class':"form-control",}
         )
     Datazione = StringField("Datazione",
-            validators=[ validators.Length(max=500)],render_kw={'class':"form-control",}
+            validators=[ ],render_kw={'class':"form-control",}
         )
     Contenuto = StringField("Contenuto",
             render_kw={'class':"form-control",}
         )
     Posizione = StringField("Posizione",
-            validators=[ validators.Length(max=500)],render_kw={'class':"form-control",}
+            validators=[ ],render_kw={'class':"form-control",}
         )
     Tipologia_scrittura = StringField("Tipologia scrittura",
-            validators=[ validators.Length(max=500)],render_kw={'class':"form-control",}
+            validators=[ ],render_kw={'class':"form-control",}
         )
     Descrizione_Esterna_Segnatura = StringField("Descrizione esterna segnatura",
-            validators=[ validators.Length(max=500)],render_kw={'class':"form-control",}
+            validators=[ ],render_kw={'class':"form-control",}
         )
 
 class Copisti(Form):
@@ -122,19 +122,19 @@ class Copisti(Form):
     it is never used by itself.
     """
     id_cop = StringField("Id copisti",
-                         validators=[validators.Length(max=500)], render_kw={'class': "form-control"}
+                         validators=[], render_kw={'class': "form-control"}
                          )
     intervallo_carte = StringField("Intervallo carte",
-                                   validators=[validators.Length(max=500)], render_kw={'class': "form-control"}
+                                   validators=[], render_kw={'class': "form-control"}
                                    )
     datazione = StringField("Datazione",
-                            validators=[validators.Length(max=500)], render_kw={'class': "form-control"}
+                            validators=[], render_kw={'class': "form-control"}
                             )
     tipologia_scrittura = StringField("Tipologia scrittura",
-                                      validators=[validators.Length(max=500)], render_kw={'class': "form-control"}
+                                      validators=[], render_kw={'class': "form-control"}
                                       )
     Descrizione_Esterna_Segnatura = StringField("Descrizione esterna segnatura",
-                                                validators=[validators.Length(max=500)], render_kw={'class': "form-control"}
+                                                validators=[], render_kw={'class': "form-control"}
                                                 )
 
 
@@ -145,13 +145,13 @@ class DescInt(Form):
     it is never used by itself.
     """
     autore = StringField("Autore",
-                         validators=[validators.Length(max=500)], render_kw={'class': "form-control", }
+                         validators=[], render_kw={'class': "form-control", }
                          )
     titolo = StringField("Titolo",
-                         validators=[validators.Length(max=500)], render_kw={'class': "form-control", }
+                         validators=[], render_kw={'class': "form-control", }
                          )
     descid = StringField("ID descrizione interna padre",
-                         validators=[validators.Length(max=500)], render_kw={'class': "form-control", }
+                         validators=[], render_kw={'class': "form-control", }
                          )
     incipit = StringField("Incipit",
                           render_kw={'class': "form-control", }
@@ -162,60 +162,60 @@ class DescInt(Form):
                             render_kw={'class': "form-control", }
                            )
     carte = StringField("Carte",
-                        validators=[validators.Length(max=500)], render_kw={'class': "form-control", }
+                        validators=[], render_kw={'class': "form-control", }
                         )
     rubrica = StringField("Rubrica",
                           render_kw={'class': "form-control", }
                           )
     Descrizione_Esterna_Segnatura = StringField("Descrizione esterna segnatura",
-                                                validators=[validators.Length(max=500)], render_kw={'class': "form-control", }
+                                                validators=[], render_kw={'class': "form-control", }
                                                 )
     Descrizione_interna_id = StringField("Descrizione interna ID",
-                                         validators=[validators.Length(max=500)], render_kw={'class': "form-control", }
+                                         validators=[], render_kw={'class': "form-control", }
                                          )
 
 class DescEst(Form):
     Segnatura = StringField("Segnatura",
-                            validators=[validators.Length(max=500)], render_kw={'class': "form-control", }
+                            validators=[], render_kw={'class': "form-control", }
                             )
     datazione = StringField("Datazione",
-                            validators=[validators.Length(max=500)], render_kw={'class': "form-control", }
+                            validators=[], render_kw={'class': "form-control", }
                             )
     tipo_di_supporto_e_qualita = StringField("Tipo di supporto e qualita",
-                                             validators=[validators.Length(max=500)], render_kw={'class': "form-control", }
+                                             validators=[], render_kw={'class': "form-control", }
                                              )
     consistenza = StringField("Consistenza",
-                              validators=[validators.Length(max=500)], render_kw={'class': "form-control", }
+                              validators=[], render_kw={'class': "form-control", }
                               )
     numerazione_carte = StringField("Numerazione carte",
-                                    validators=[validators.Length(max=500)], render_kw={'class': "form-control", }
+                                    validators=[], render_kw={'class': "form-control", }
                                     )
     carte_di_guardia = StringField("Carte di guardia",
-                                   validators=[validators.Length(max=500)], render_kw={'class': "form-control", }
+                                   validators=[], render_kw={'class': "form-control", }
                                    )
     prospetto_fascicolazione = StringField("Prospetto fascicolazione",
-                                           validators=[validators.Length(max=500)], render_kw={'class': "form-control", }
+                                           validators=[], render_kw={'class': "form-control", }
                                            )
     arrangiamento_fogli_gregory = StringField("Arrangiamento fogli gregory",
-                                              validators=[validators.Length(max=500)], render_kw={'class': "form-control", }
+                                              validators=[], render_kw={'class': "form-control", }
                                               )
     dimensioni = StringField("Dimensioni",
-                             validators=[validators.Length(max=500)], render_kw={'class': "form-control", }
+                             validators=[], render_kw={'class': "form-control", }
                              )
     rigatura = StringField("Rigatura",
-                           validators=[validators.Length(max=500)], render_kw={'class': "form-control", }
+                           validators=[], render_kw={'class': "form-control", }
                            )
     foratura = StringField("Foratura",
-                           validators=[validators.Length(max=500)], render_kw={'class': "form-control", }
+                           validators=[], render_kw={'class': "form-control", }
                            )
     legatura = StringField("Legatura",
-                           validators=[validators.Length(max=500)], render_kw={'class': "form-control", }
+                           validators=[], render_kw={'class': "form-control", }
                            )
     utenti_email = StringField("Utenti email",
-                               validators=[validators.Length(max=500)], render_kw={'class': "form-control", }
+                               validators=[], render_kw={'class': "form-control", }
                                )
     Descrizione_Esterna_Segnatura = StringField("Descrizione esterna segnatura",
-                                                validators=[validators.Length(max=500)], render_kw={'class': "form-control", }
+                                                validators=[], render_kw={'class': "form-control", }
                                                 )
 class MainForm(FlaskForm):
     """Parent form."""
